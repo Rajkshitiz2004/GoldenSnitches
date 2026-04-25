497ce47b76e3abd04b6dc7ab37266478fbc51812
Anshika <Anshikavermaav232006@gmail.com>
2026-04-25 17:31:30 +0530

    update project files

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
-import Dashboard from "./pages/Dashboard";
-
-export default function App() {
-  return <Dashboard />;
-}
+import { Route, Routes } from "react-router-dom"
+import About from "./pages/About"
+import Skills from "./pages/Skills"
+export default function App(){
+  return (
+    <div>
+      My portfolio
+     <Routes>
+  <Route path="dashboard" element={<Dashboard />}>
+    <Route index element={<Home />} />
+    <Route path="settings" element={<Settings />} />
+  </Route>
+</Routes>
+    </div>
+  )
+} 
